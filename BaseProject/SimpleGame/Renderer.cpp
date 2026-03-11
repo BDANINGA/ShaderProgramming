f516ef1650dee6237bb8bd9fd00ad83457d1d072
DESKTOP-QCIEGQR\임성훈 <cmw0210@naver.com>
2026-03-11 10:17:16 +0900

    first

---
+++ BaseProject/SimpleGame/Renderer.cpp
@@ -0,0 +1,189 @@
+#include "stdafx.h"
+#include "Renderer.h"
+
+Renderer::Renderer(int windowSizeX, int windowSizeY)
+{
+	Initialize(windowSizeX, windowSizeY);
+}
+
+
+Renderer::~Renderer()
+{
+}
+
+void Renderer::Initialize(int windowSizeX, int windowSizeY)
+{
+	//Set window size
+	m_WindowSizeX = windowSizeX;
+	m_WindowSizeY = windowSizeY;
+
+	//Load shaders
+	m_SolidRectShader = CompileShaders("./Shaders/SolidRect.vs", "./Shaders/SolidRect.fs");
+	
+	//Create VBOs
+	CreateVertexBufferObjects();
+
+	if (m_SolidRectShader > 0 && m_VBORect > 0)
+	{
+		m_Initialized = true;
+	}
+}
+
+bool Renderer::IsInitialized()
+{
+	return m_Initialized;
+}
+
+void Renderer::CreateVertexBufferObjects()
+{
+	float rect[]
+		=
+	{
+		-1.f / m_WindowSizeX, -1.f / m_WindowSizeY, 0.f, -1.f / m_WindowSizeX, 1.f / m_WindowSizeY, 0.f, 1.f / m_WindowSizeX, 1.f / m_WindowSizeY, 0.f, //Triangle1
+		-1.f / m_WindowSizeX, -1.f / m_WindowSizeY, 0.f,  1.f / m_WindowSizeX, 1.f / m_WindowSizeY, 0.f, 1.f / m_WindowSizeX, -1.f / m_WindowSizeY, 0.f, //Triangle2
+	};
+
+	glGenBuffers(1, &m_VBORect);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBORect);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(rect), rect, GL_STATIC_DRAW);
+}
+
+void Renderer::AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+	//���̴� ������Ʈ ����
+	GLuint ShaderObj = glCreateShader(ShaderType);
+
+	if (ShaderObj == 0) {
+		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+	}
+
+	const GLchar* p[1];
+	p[0] = pShaderText;
+	GLint Lengths[1];
+	Lengths[0] = strlen(pShaderText);
+	//���̴� �ڵ带 ���̴� ������Ʈ�� �Ҵ�
+	glShaderSource(ShaderObj, 1, p, Lengths);
+
+	//�Ҵ�� ���̴� �ڵ带 ������
+	glCompileShader(ShaderObj);
+
+	GLint success;
+	// ShaderObj �� ���������� ������ �Ǿ����� Ȯ��
+	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		GLchar InfoLog[1024];
+
+		//OpenGL �� shader log �����͸� ������
+		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+		printf("%s \n", pShaderText);
+	}
+
+	// ShaderProgram �� attach!!
+	glAttachShader(ShaderProgram, ShaderObj);
+}
+
+bool Renderer::ReadFile(char* filename, std::string *target)
+{
+	std::ifstream file(filename);
+	if (file.fail())
+	{
+		std::cout << filename << " file loading failed.. \n";
+		file.close();
+		return false;
+	}
+	std::string line;
+	while (getline(file, line)) {
+		target->append(line.c_str());
+		target->append("\n");
+	}
+	return true;
+}
+
+GLuint Renderer::CompileShaders(char* filenameVS, char* filenameFS)
+{
+	GLuint ShaderProgram = glCreateProgram(); //�� ���̴� ���α׷� ����
+
+	if (ShaderProgram == 0) { //���̴� ���α׷��� ����������� Ȯ��
+		fprintf(stderr, "Error creating shader program\n");
+	}
+
+	std::string vs, fs;
+
+	//shader.vs �� vs ������ �ε���
+	if (!ReadFile(filenameVS, &vs)) {
+		printf("Error compiling vertex shader\n");
+		return -1;
+	};
+
+	//shader.fs �� fs ������ �ε���
+	if (!ReadFile(filenameFS, &fs)) {
+		printf("Error compiling fragment shader\n");
+		return -1;
+	};
+
+	// ShaderProgram �� vs.c_str() ���ؽ� ���̴��� �������� ����� attach��
+	AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+
+	// ShaderProgram �� fs.c_str() �����׸�Ʈ ���̴��� �������� ����� attach��
+	AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+	GLint Success = 0;
+	GLchar ErrorLog[1024] = { 0 };
+
+	//Attach �Ϸ�� shaderProgram �� ��ŷ��
+	glLinkProgram(ShaderProgram);
+
+	//��ũ�� �����ߴ��� Ȯ��
+	glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+
+	if (Success == 0) {
+		// shader program �α׸� �޾ƿ�
+		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+		std::cout << filenameVS << ", " << filenameFS << " Error linking shader program\n" << ErrorLog;
+		return -1;
+	}
+
+	glValidateProgram(ShaderProgram);
+	glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+	if (!Success) {
+		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+		std::cout << filenameVS << ", " << filenameFS << " Error validating shader program\n" << ErrorLog;
+		return -1;
+	}
+
+	glUseProgram(ShaderProgram);
+	std::cout << filenameVS << ", " << filenameFS << " Shader compiling is done.";
+
+	return ShaderProgram;
+}
+
+void Renderer::DrawSolidRect(float x, float y, float z, float size, float r, float g, float b, float a)
+{
+	float newX, newY;
+
+	GetGLPosition(x, y, &newX, &newY);
+
+	//Program select
+	glUseProgram(m_SolidRectShader);
+
+	glUniform4f(glGetUniformLocation(m_SolidRectShader, "u_Trans"), newX, newY, 0, size);
+	glUniform4f(glGetUniformLocation(m_SolidRectShader, "u_Color"), r, g, b, a);
+
+	int attribPosition = glGetAttribLocation(m_SolidRectShader, "a_Position");
+	glEnableVertexAttribArray(attribPosition);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBORect);
+	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glDisableVertexAttribArray(attribPosition);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void Renderer::GetGLPosition(float x, float y, float *newX, float *newY)
+{
+	*newX = x * 2.f / m_WindowSizeX;
+	*newY = y * 2.f / m_WindowSizeY;
+}
